4db871a60f7f5483103fb65772edd1fa7a69c7c3
Gabriel Leite <gabriel@convertcompany.com.br>
2023-04-02 17:49:50 -0300

    🔧 chore: mensagens em portugues

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,19 +4,26 @@ import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 import { ClerkProvider, SignedIn, SignedOut, SignIn } from "@clerk/nextjs";
+import { ptBR } from "~/utils/localization";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <ClerkProvider {...pageProps}>
+    <ClerkProvider {...pageProps} localization={ptBR}>
       <SignedIn>
         <Component {...pageProps} />
       </SignedIn>
       <SignedOut>
-        <div className="flex h-screen w-screen items-center justify-center bg-sky-50">
-          <SignIn />
-        </div>
+        <Login/>
       </SignedOut>
     </ClerkProvider>
+  );
+};
+
+const Login = () => {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center bg-sky-50">
+      <SignIn />
+    </div>
   );
 };
 
