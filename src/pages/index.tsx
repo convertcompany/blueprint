4bceb70e7252e45c39c030ec3abf96efd1a2ef0a
Gabriel Leite <gabriel@convertcompany.com.br>
2023-04-03 17:58:13 -0300

    ✨ feat: teste de cadastro

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,6 +15,12 @@ type BlueprintProps = RouterOutputs["blueprints"]["getAll"][number];
 const Home: NextPage = () => {
   const user = useUser();
   const { data, isLoading } = api.blueprints.getAll.useQuery();
+  const { mutate } = api.blueprints.create.useMutation();
+
+  const createBlueprint = async () => {
+    const blueprint = await mutate({ name: "Novo Projeto", description: "Descrição do projeto" });
+    console.log(blueprint);
+  };
 
   return (
     <>
@@ -42,7 +48,7 @@ const Home: NextPage = () => {
           ) : (
             <div className="flex flex-col gap-6">
               <div className="flex gap-6 transition-all">
-                <div className="relative flex grow select-none items-center gap-4 overflow-hidden rounded-2xl border border-slate-200 bg-white p-4 antialiased shadow-sm outline-none ring-blue-500 ring-offset-2 transition-all hover:shadow-md focus-visible:ring-2 active:shadow-none" tabIndex={0}>
+                <div onClick={createBlueprint} className="relative flex grow select-none items-center gap-4 overflow-hidden rounded-2xl border border-slate-200 bg-white p-4 antialiased shadow-sm outline-none ring-blue-500 ring-offset-2 transition-all hover:shadow-md focus-visible:ring-2 active:shadow-none" tabIndex={0}>
                   <div className="relative">
                     <Image alt="Icone" width={24} height={24} src={"/logos/icon.svg"} className="grayscale" />
                     <IoMdAddCircle className="text-md absolute left-4 top-3 rounded-full bg-white text-blue-500" />
@@ -77,7 +83,7 @@ const BlueprintCard = (blueprint: BlueprintProps) => {
           <div className="mt-2 flex items-center gap-2">
             <Image alt="Imagem" src={blueprint?.author?.profileImageUrl ?? ""} className="rounded-full" width={20} height={20} />
             <p className="text-xs font-medium text-slate-500 ">
-              Criado por {blueprint.author.fullName} • {dayjs(blueprint.createdAt).fromNow()}
+              por {blueprint.author.fullName} • {dayjs(blueprint.createdAt).fromNow()}
             </p>
           </div>
         )}
