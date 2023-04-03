fc1430aa6ea0a87fd03bc1dbe47828fd78fa77d6
Gabriel Leite <gabriel@convertcompany.com.br>
2023-04-03 10:00:54 -0300

    ♻️ refactor: deletei os modulos de exemplo

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,4 @@
 import { createTRPCRouter } from "~/server/api/trpc";
-import { exampleRouter } from "~/server/api/routers/example";
 import { blueprintsRouter } from "~/server/api/routers/blueprint";
 
 /**
@@ -8,7 +7,6 @@ import { blueprintsRouter } from "~/server/api/routers/blueprint";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
   blueprints: blueprintsRouter,
 });
 
